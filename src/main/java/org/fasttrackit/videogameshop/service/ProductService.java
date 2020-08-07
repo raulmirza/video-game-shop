d20737677843bb41d6e242720d8d2163907ab1c7
maraalexandra <mara.alexandra11@yahoo.com>
2020-08-07 16:00:06 +0300

    added logger in product service

---
+++ src/main/java/org/fasttrackit/videogameshop/service/ProductService.java
@@ -0,0 +1,36 @@
+package org.fasttrackit.videogameshop.service;
+
+import org.fasttrackit.videogameshop.domain.Product;
+import org.fasttrackit.videogameshop.persistance.ProductRepository;
+import org.fasttrackit.videogameshop.transfer.SaveProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ProductService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(SaveProductRequest request) {
+        System.out.println("Creating product: " + request);
+        LOGGER.info("Creating product {}", request);
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setQuantity(request.getQuantity());
+        product.setImageUrl(request.getImageUrl());
+
+       return productRepository.save(product);
+    }
+}
